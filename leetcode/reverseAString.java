657d395d5ccfd33b76f90c65677a979b555e17a3
Arman-ali-khan-786 <alikhanarman786@gmail.com>
2023-03-19 10:22:38 +0530

    reverse a String inplace

--- leetcode/reverseAString.java
+++ leetcode/reverseAString.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
 public class reverseAString {
     public void reverseString(char[] s) {
         int start = 0, end = s.length - 1;
