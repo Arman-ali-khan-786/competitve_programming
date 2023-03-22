e6d19ac8911d380fe21f68cb943a40c6311229c5
Arman-ali-khan-786 <alikhanarman786@gmail.com>
2023-03-22 17:39:46 +0530

    to find the maximum amount you can get if you perform zero or more such operations optimally

---
+++ gfg/potd/String_rp_or_pr.py
@@ -0,0 +1,35 @@
+############################# PROBLEM OF THE DAY - 22th March 2023 #################################################
+#-------------------------------------------------------------------------------------------------------------------
+
+
+# Given a string S. In one operation, you can remove the substring "pr" from the string S and get amount X or 
+# you can remove the substring "rp" and get the amount Y. 
+# Find the maximum amount you can get if you perform zero or more such operations optimally. 
+
+
+class Solution:
+    def solve (self, X, Y, S):
+        #code here
+        x,y = 0,0
+        while(True):
+            if(X>=Y):
+                if('pr'in S):
+                    x +=S.count('pr')
+                    S=S.replace('pr','')
+                elif('rp' in S):
+                    y+=S.count('rp')
+                    S=S.replace('rp','')
+                else:
+                    break
+                
+            if(Y>X):
+                if('rp'in S):
+                    y+=S.count('rp')
+                    S=S.replace('rp','')
+                elif('pr'in S):
+                    x+=S.count('pr')
+                    S=S.replace('pr','')
+                else:
+                    break
+            
+        return (X*x)+(y*Y)
