872a22978cf00141aa679c839cc506247f106c81
Arman-ali-khan-786 <alikhanarman786@gmail.com>
2023-03-26 21:22:44 +0530

    updated with comments

--- gfg/potd/frogsAndJump.java
+++ gfg/potd/frogsAndJump.java
@@ -1,3 +1,15 @@
+//############################# PROBLEM OF THE DAY - 26th March 2023 #################################################
+// -------------------------------------------------------------------------------------------------------------------
+
+
+// N frogs are positioned at one end of the pond. All frogs want to reach the other end of the pond as soon as possible. 
+// The pond has some leaves arranged in a straight line. Each frog has the strength to jump exactly K leaves.
+//  For example, a  frog having strength 2 will visit the leaves 2, 4, 6, ...  etc. while crossing the pond.
+
+// Given the strength of each frog and the number of leaves, 
+// your task is to find the number of leaves that not be visited by any of the 
+// frogs when all frogs have reached the other end of the pond. 
+
 package potd;
 import java.util.Arrays;
 
