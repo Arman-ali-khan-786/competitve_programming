e7df0df07b7fa29f9298910a59e4830b81050347
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2023-06-01 20:14:31 +0530

    to find Peak Element

---
+++ leetcode/findPeakElement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int s = 0 , e = nums.length-1;
+        while(s<e){
+            int mid = s + (e-s)/2;
+            if(nums[mid] > nums[mid+1]) e = mid;
+            else if(nums[mid] < nums[mid+1]) s = mid+1;
+        }
+        return s;
+    }
+}
