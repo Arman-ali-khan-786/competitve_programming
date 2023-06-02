3b241a06a785cdcc7a24da43aeccfcfb52bb7485
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2023-06-02 06:51:45 +0530

    to search 2D Array.

---
+++ leetcode/search2DArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = 0;
+        int c = matrix[0].length-1;
+        while(r<matrix.length && c>=0){
+            if(matrix[r][c] == target) return true;
+            if(target < matrix[r][c]) c--;
+            else r++;
+        }
+        return false;
+    }
+}
